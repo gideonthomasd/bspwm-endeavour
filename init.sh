d336d7e9075eb8bc989240ad9e6cc3e265dfe7bd
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2021-04-20 19:10:49 +0100

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+
+sudo pacman -Syyu --noconfirm
+sudo pacman -S jgmenu youtube-dl pcmanfm lxtask vlc --noconfirm
+
+cd bspwm
+cd scripts
+chmod +x *.sh
+cd i3lock-fancy
+chmod +x *.sh
+cd ..
+cd ..
+cd ..
+
+cd polybar
+chmod +x *.sh
+cd scripts
+chmod +x *.sh
+cd ..
+cd ..
+
+
+
+cd polybar
+cp -r * ~/.config/polybar
+cd ..
+
+cd termite
+cp -r * ~/.config/termite
+cd ..
+
+cp bashrc ~/.bashrc
+
+jgmenu_run init --theme=col3
+
+cd jgmenu
+cp -r * ~/.config/jgmenu
+cd ..
+
+cd bspwm
+cp -r * ~/.config/bspwm
+cd ..
+
+cd shxkd
+cp -r * ~/.config/sxhkd
+cd ..
+
+
+yay -S --noconfirm timeshift brave-bin pamac-all
